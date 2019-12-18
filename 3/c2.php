4b182981a5b82a39b8ef70bb433487c5b7f00b1f
foreman grillo <trogmaiu@gmail.com>
2019-12-18 15:35:54 -0300

    IDE?A

--- 3/c2.php
+++ 3/c2.php
@@ -6,27 +6,6 @@ $write;
 $i = 1;
 $t;
  while ($i <= 85 ) {
-if ( $i  == $t ) {
-echo "<tr>";
-echo "<td>";
-echo "a$i";
-echo "</td>"
-echo "</tr>"
-}
-}
-echo "</table>"
-?>
-</body>
-</html>
-
-<html>
-<title> Lista</title>
-<body>
-<?php
-$write;
-$i = 1;
-$t;
- while ($i <= 85 ) {
 echo "<tr>";
 echo "<td>";
 echo "a$i";
